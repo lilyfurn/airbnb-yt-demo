41c7742b815d19c762a9dd3d37ce31379a176e39
andrew <andrew@lilyfurn.com>
2022-04-27 15:06:37 -0500

    day-one-build

---
+++ pages/index.js
@@ -0,0 +1,75 @@
+import Head from 'next/head'
+import Image from 'next/image'
+import Header from '../components/Header'
+import Banner from '../components/Banner'
+import SmallCard from '../components/SmallCard'
+import MediumCard from '../components/MediumCard'
+import LargeCard from '../components/LargeCard'
+import Footer from '../components/Footer'
+
+export default function Home({ exploreData, cardsData }) {
+  return (
+    <div className="">
+      <Head>
+        <title>Airbnb-clone</title>
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+      <Header />
+      <Banner />
+      <main className="mx-auto max-w-7xl px-8 sm:px-16">
+        <section className="pt-6">
+          <h2 className="pb-5 text-4xl font-semibold">Explore Nearby</h2>
+
+          {/* pull some data */}
+          <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 xl:grid-cols-5">
+            {exploreData?.map(({ img, location, distance }) => (
+              <SmallCard
+                key={img}
+                img={img}
+                distance={distance}
+                location={location}
+              />
+            ))}
+          </div>
+        </section>
+
+        <section>
+          <h2 className="py-8 text-4xl font-semibold">Live Anywhere</h2>
+
+          <div className="-ml-3 flex space-x-3 overflow-scroll p-3 scrollbar-hide">
+            {cardsData?.map(({ img, title }) => (
+              <MediumCard key={img} img={img} title={title} />
+            ))}
+          </div>
+        </section>
+
+        <section>
+          <LargeCard
+            img="https://links.papareact.com/4cj"
+            title="The Greatest Outdoors"
+            description="Wishlist curated by Airbnb."
+            buttonText="Get Inspired"
+          />
+        </section>
+      </main>
+      <Footer />
+    </div>
+  )
+}
+
+export async function getStaticProps() {
+  const exploreData = await fetch('https://links.papareact.com/pyp').then(
+    (res) => res.json()
+  )
+
+  const cardsData = await fetch('https://links.papareact.com/zp1').then((res) =>
+    res.json()
+  )
+
+  return {
+    props: {
+      exploreData,
+      cardsData,
+    },
+  }
+}
